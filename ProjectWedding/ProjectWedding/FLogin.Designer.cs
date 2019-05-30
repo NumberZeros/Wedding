040de3d6e4eb0322929350a703e54e58ddc9edec
Axel <17520247@gm.uit.edu.vn>
2019-05-30 22:56:55 +0700

    chỉnh lại form login

--- ProjectWedding/ProjectWedding/FLogin.Designer.cs
+++ ProjectWedding/ProjectWedding/FLogin.Designer.cs
@@ -28,57 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogin));
             this.tbAccount = new System.Windows.Forms.TextBox();
             this.tbPass = new System.Windows.Forms.TextBox();
             this.btLogin = new System.Windows.Forms.Button();
-            this.tbExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tbAccount
             // 
-            this.tbAccount.Location = new System.Drawing.Point(285, 80);
+            this.tbAccount.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAccount.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbAccount.Location = new System.Drawing.Point(214, 85);
+            this.tbAccount.Margin = new System.Windows.Forms.Padding(2);
             this.tbAccount.Name = "tbAccount";
-            this.tbAccount.Size = new System.Drawing.Size(100, 22);
+            this.tbAccount.Size = new System.Drawing.Size(137, 26);
             this.tbAccount.TabIndex = 0;
+            this.tbAccount.Text = "Username";
             // 
             // tbPass
             // 
-            this.tbPass.Location = new System.Drawing.Point(285, 153);
+            this.tbPass.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPass.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbPass.Location = new System.Drawing.Point(214, 144);
+            this.tbPass.Margin = new System.Windows.Forms.Padding(2);
             this.tbPass.Name = "tbPass";
-            this.tbPass.Size = new System.Drawing.Size(100, 22);
+            this.tbPass.Size = new System.Drawing.Size(137, 26);
             this.tbPass.TabIndex = 1;
+            this.tbPass.Tag = "";
+            this.tbPass.Text = "Password";
             // 
             // btLogin
             // 
-            this.btLogin.Location = new System.Drawing.Point(285, 243);
+            this.btLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLogin.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLogin.Location = new System.Drawing.Point(214, 194);
+            this.btLogin.Margin = new System.Windows.Forms.Padding(2);
             this.btLogin.Name = "btLogin";
-            this.btLogin.Size = new System.Drawing.Size(75, 36);
+            this.btLogin.Size = new System.Drawing.Size(137, 32);
             this.btLogin.TabIndex = 2;
-            this.btLogin.Text = "Login ";
+            this.btLogin.Text = "LOGIN";
             this.btLogin.UseVisualStyleBackColor = true;
             this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
             // 
-            // tbExit
+            // pictureBox1
             // 
-            this.tbExit.Location = new System.Drawing.Point(285, 319);
-            this.tbExit.Name = "tbExit";
-            this.tbExit.Size = new System.Drawing.Size(75, 23);
-            this.tbExit.TabIndex = 3;
-            this.tbExit.Text = "Exit";
-            this.tbExit.UseVisualStyleBackColor = true;
-            this.tbExit.Click += new System.EventHandler(this.tbExit_Click);
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(177, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 23);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(177, 85);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(22, 23);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // FLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tbExit);
+            this.ClientSize = new System.Drawing.Size(580, 370);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btLogin);
             this.Controls.Add(this.tbPass);
             this.Controls.Add(this.tbAccount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FLogin";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,7 +121,8 @@
         private System.Windows.Forms.TextBox tbAccount;
         private System.Windows.Forms.TextBox tbPass;
         private System.Windows.Forms.Button btLogin;
-        private System.Windows.Forms.Button tbExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
